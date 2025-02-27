960201b65321b435d44d35f2430cda410fcfd04d
mattfanlo <mmfanlo@mcm.edu.ph>
2025-02-28 02:56:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = 'cbb0b276f1724b6fb1420151241811'; 
+const city = 'Davao'; 
+
+document.getElementById('fetchWeatherButton').addEventListener('click', callAPI);
+
+
+function callAPI() {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+
+            document.querySelector("#weather_city").textContent = data.location.name || 'N/A';
+            document.querySelector("#weather_temperature").textContent = `${data.current.temp_c}°C` || 'N/A';
+            document.querySelector("#weather_condition").textContent = data.current.condition.text || 'N/A';
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+      
+            document.querySelector("#weather_city").textContent = "Error fetching data";
+            document.querySelector("#weather_temperature").textContent = "N/A";
+            document.querySelector("#weather_condition").textContent = "N/A";
+        });
+}
